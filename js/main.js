d82b6a4b90b05d212c6d156fd75f5f4e0020d1bc
clopez11 <char.lopez11@gmail.com>
2017-02-21 19:56:27 -0500

    my first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(function () {
+    
+//   var today = new Date();
+//     weddingDate = new Date(today.getFullYear() + 1, 0, 1, 0, 0, 0, 0);
+    
+//   $('#countdown').countdown({
+//     until: weddingDate
+//   });
+
+// });
